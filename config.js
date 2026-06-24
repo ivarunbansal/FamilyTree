984f93c7d29335eadd0125ae9ded61e4be63a022
VARUN BANSAL <varunb270@gmail.com>
2026-06-24 23:32:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.FAMILY_TREE_CONFIG = {
   googleSheetUrl: "https://docs.google.com/spreadsheets/d/1YpCkLsT6G1qGwdTuyiUpnRoSHKMJMXhGfr6oyshsBm8/edit?usp=sharing",
-  siteTitle: "Gupta Bansal Family Tree",
+  siteTitle: "Badli Bansal Family Tree",
   theme: {
     defaultMode: "dark",
     accentColor: "#9f7aea",
